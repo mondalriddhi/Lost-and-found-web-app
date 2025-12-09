97c3d695554d2586b000031752138286e7fb2863
unknown <gryfrid5972@gmail.com>
2025-12-09 21:22:11 +0530

    whitelisted vercel

--- lostAndFoundBackend/lostAndFoundBackend/src/main/java/com/rmondal/lostAndFoundBackend/config/SecurityConfig.java
+++ lostAndFoundBackend/lostAndFoundBackend/src/main/java/com/rmondal/lostAndFoundBackend/config/SecurityConfig.java
@@ -43,7 +43,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Allow React Frontend
+        configuration.setAllowedOrigins(List.of("http://localhost:5173",
+                "https://lost-and-found-web-app-frontend.vercel.app")); // Allow React Frontend
+
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
